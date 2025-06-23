7a8bb2cd04ecd51125b26ba2916b3d90a078fef6
Gabriel Balduíno <gabrielbalduino29@gmail.com>
2025-06-23 18:03:43 -0300

    5 react router

---
+++ 5_react_router/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1>Sobre a empresa</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ 5_react_router/src/components/Settings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Settings() {
+  return (
+    <div>
+        <h3>Configurações</h3>
+    </div>
+  )
+}
+
+export default Settings
